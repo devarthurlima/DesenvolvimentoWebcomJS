c73f8a051ce57af6cfcd56acbc373142a072a45d
Arthur Lima <arthur.lima3@icloud.com>
2026-06-02 20:38:31 -0300

    Iniciando o módulo de JavaScript

---
+++ JAVASCRIPT/helloWorld.js
@@ -0,0 +1,3 @@
+console.log("Hello, World!");
+
+console.log("Essa é a minha primeira aula de JavaScript!");
---
+++ JAVASCRIPT/variaveis.js
@@ -0,0 +1,22 @@
+// É uma estrutura de dados que armazena um valor e pode ser alterada durante a execução do programa.
+
+var nome = "Arthur Lima";
+var idade = 18;
+var altura = 1.70;
+var peso = 73;
+var ehDoadorDeSangue = true;
+var tipoSanguineo = "A";
+var fatorRh = "+";
+
+// Usando consoles diferentes
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Altura: " + altura);
+console.log("Peso: " + peso);
+console.log("É doador de sangue: " + ehDoadorDeSangue);
+console.log("Tipo sanguíneo: " + tipoSanguineo + fatorRh);
+
+console.log("\n");
+
+// Usando um único console
+console.log("Nome: " + nome + "\nIdade: " + idade + "\nAltura: " + altura + "\nPeso: " + peso + "\nÉ doador de sangue: " + ehDoadorDeSangue + "\nTipo sanguíneo: " + tipoSanguineo + fatorRh);
